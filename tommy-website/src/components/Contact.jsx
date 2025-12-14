a9ebc916ce944db77b0fc32abf37fc206ca73d28
Eyal Yehuda <eyal.yehuda11@gmail.com>
2025-12-14 14:48:17 +0100

    get in touch part added (contact)

--- tommy-website/src/components/Contact.jsx
+++ tommy-website/src/components/Contact.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import "./Contact.css";
+
+
+
 function Contract(){
-    
+     return(
+        <section id="contact" className="contact">
+            <div className="contact-inner">
+                <h1 className="contact-title">Get In Touch</h1>
+                <p className="contact-under-title">Got an idea that's waiting to come alive? lets build it together</p>
+                <div className="address-details">
+                    <h3>
+                        <span>Tommy Complex</span>
+                        <span>Erich-Weinert-Straße 22</span>
+                        <span>10439 Berlin</span>
+                    </h3>
+                    <h3>
+                        <span>+49 (0) 176 7901 4103</span>
+                        <span>tommycomplexlight@gmail.com</span>
+                    </h3>
+                </div>
+
+                
+            </div>
+
+        </section>
+    )
 }
 export default Contract
--- tommy-website/src/components/NavBar.jsx
+++ tommy-website/src/components/NavBar.jsx
@@ -45,7 +45,7 @@ function NavBar() {
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#recommendations">Recommendations</a></li>
-          <li><a href="#contact">Resume</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
     </nav>
