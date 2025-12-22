7b6c8942ea80f7806b2992121c461a8530a65873
Eyal Yehuda <eyal.yehuda11@gmail.com>
2025-12-22 14:46:35 +0100

    pdf, symbols and social media links

--- tommy-website/src/components/Contact.jsx
+++ tommy-website/src/components/Contact.jsx
@@ -21,8 +21,13 @@ function Contract(){
                         <span>tommycomplexlight@gmail.com</span>
                     </h3>
                 </div>
-
                 
+                
+            </div>
+            <div className="buttons">
+                <a href="https://www.facebook.com/share/1CBUE7Eyji/?mibextid=wwXIfr">Facebook</a>
+                <a href="https://www.instagram.com/tommycomplex?igsh=MWJmZnJxN2JhYXJoMw%3D%3D&utm_source=qr">Instagram</a>
+                <a  href="https://www.linkedin.com/in/tommy-complex-3a5a63230?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Linkdin</a>   
             </div>
 
         </section>
--- tommy-website/src/components/Recommendation.jsx
+++ tommy-website/src/components/Recommendation.jsx
@@ -1,6 +1,15 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import "./Recommendation.css";
+import simulSymbol from "../assets/Simul_Symbol.png";
+import macroSymbol from "../assets/Macromedia_Symbol.png";
+import fishbowlSymbol from "../assets/Fishbowl_Symbol.png";
+import slaughterSymbol from "../assets/Slaughter_Symbol.png";
+
+import fishbowlPdf from "../assets/recommendations/fishbowl.pdf";
+import macroPdf from "../assets/recommendations/macromedia.pdf";
+import simulPdf from "../assets/recommendations/simul.pdf";
+
 
 
 function Recommendation(){
@@ -9,6 +18,45 @@ function Recommendation(){
             <div className="recommendations-inner">
                 <h1 className="recommendations-title">Recommendations</h1>
             </div>
+
+
+            <a
+                 href="https://example.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                src={slaughterSymbol} alt="Slaughter House symbol" className="simul-image"/>
+            </a>
+
+            <a
+                 href={fishbowlPdf}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                src={fishbowlSymbol} alt="The Fishbowl symbol" className="simul-image"/>
+            </a>
+
+            <a
+                 href={macroPdf}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                src={macroSymbol} alt="Macromedia symbol" className="simul-image"/>
+            </a>
+
+    
+
+             <a
+                 href={simulPdf}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                src={simulSymbol} alt="Simul symbol" className="simul-image"/>
+            </a>
 
         </section>
     )
